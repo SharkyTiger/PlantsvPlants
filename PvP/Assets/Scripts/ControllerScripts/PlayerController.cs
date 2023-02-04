64df3661f417668f434c7350a4390c67207db33f
Dennis Leistikow <dennisl03@web.de>
2023-02-04 13:34:59 +0100

    Sorting Order gefixt

--- PvP/Assets/Scripts/ControllerScripts/PlayerController.cs
+++ PvP/Assets/Scripts/ControllerScripts/PlayerController.cs
@@ -10,20 +10,26 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public GameObject PrefabLeft;
     public GameObject PrefabRight;
+    public GameObject highlightPrefab;
+    private GameObject highlight;
     public Tilemap map;
+    public Tilemap interActiveMap;
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
+        highlight = Instantiate(highlightPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 vec3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        vec3.x += 0.5f;
+        vec3.y += 0.5f;
+        var cellPosition = map.LocalToCell(vec3);
+        highlight.transform.position = cellPosition;
         if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
         {
-            Vector3 vec3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            vec3.z = 1;
-            var cellPosition = map.LocalToCell(vec3);
             Debug.Log(cellPosition);
 
             var currentObject = GetGameObjectFromPosition(cellPosition);
@@ -45,13 +51,13 @@ public class PlayerController : MonoBehaviour
         }   
     }
 
-    GameObject? GetGameObjectFromPosition(Vector3 position)
+    GameObject GetGameObjectFromPosition(Vector3 position)
     {
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
         foreach (var obj in allObjects)
         {
             float dist = Vector3.Distance(position, obj.transform.position);
-            if (dist < 1)
+            if (dist < 1 && obj != highlight)
             {
                 return obj;
             }
