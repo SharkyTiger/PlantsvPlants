c4559bf27994384d797b5e1cfdf80d13ed00e42a
SharkyTiger <48879976+SharkyTiger@users.noreply.github.com>
2023-02-04 09:34:22 +0100

    Testscene Dennis + Philip erstellt

--- PvP/Assets/Scripts/Manager/UIManager.cs
+++ PvP/Assets/Scripts/Manager/UIManager.cs
@@ -1,20 +1,30 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI WaterCountText;
     public TextMeshProUGUI FertilizerCountText;
+    public Button ShopToggle;
 
     private Int32 waterCount;
     private Int32 fertilizerCount;
+
     // Start is called before the first frame update
     void Start()
     {
         waterCount = 250;
         fertilizerCount = 250;
         SetCountTexts();
+        ShopToggle.onClick.AddListener(OnShopToggle);
+    }
+
+    void OnDestroy()
+    {
+        
     }
 
     public void AddToWater(int add)
@@ -33,5 +43,11 @@ public class UIManager : MonoBehaviour
     {
         WaterCountText.text = $"W - {waterCount}";
         FertilizerCountText.text = $"F - {fertilizerCount}";
+    }
+
+    private void OnShopToggle()
+    {
+        AddToWater(Random.Range(-5, 6));
+        AddToFertilizer(Random.Range(-5, 6));
     }
 }
